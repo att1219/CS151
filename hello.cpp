111654dd23c571eff46f5c5ca5813d9f091b6296
att1219 <adamthomas@mec.science>
2019-09-04 09:37:03 -0400

    msg

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main()
 {
     std::cout << "Hello World\n";
-        return 0;
+    cout << 21 << "\n";
+    cout << 3.1415 << "\n";
+    cout << 21+32 << "\n";
+    cout << "Hello" <<" " << "World" << "\n";
+    return 0;
+    cout << "I was reached\n";
 }
