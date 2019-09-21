e05c1081f95bc2b1ac8a886f72f08e38268caeab
att1219 <adamthomas@mec.science>
2019-09-21 13:22:50 -0400

    lol

---
+++ Assignments/homework2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void Echo(string msg)
+{}
+
+int SumOfThree(int n)
+{}
+
+void SquarePerimeter(double side)
+{}
+
+void CapitalI()
+{}
+
+void NextNextCharacter(char ch)
+{}
+
+bool OddMult3(int value)
+{}
+
+void PerfectSquare()
+{}
+
+double RectangleArea(double width,double length)
+{}
+
+void Swap(x,y)
+{}
+
+double Quadratic(double a,double b,double c,double x)
+{}
+
+string Name()
+{}
+
+bool LesserAbsolute(int m,int n)
+{}
+
+void SwapFour(char r,char s,char t,char u)
+{}
+
+bool LeapYear(int year)
+{}
+
+double Distance2D(double x1,double y1,double x2,double y2)
+{}
+
+void Invite(string name1,string theme,string name2,string place,string weekday,string time,string email)
+{}
+
+void NumberSwap(x,y)
+{}
+
+void Permutation(int a,int b,int c,int d)
+{}
+
+int main()
+{
+    return 0;
+}
+
---
+++ Practice/practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+float floatTest(int x);
+{
+    
+}
+
+int main()
+{
+    return 0;
+}
+
