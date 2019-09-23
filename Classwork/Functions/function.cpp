af6e20feae342698d826a85e90634aa273f6588c
att1219 <adamthomas@mec.science>
2019-09-23 09:32:10 -0400

    lol

---
+++ Classwork/Functions/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+double Area(double r)
+{
+    return(3.1415*r*r);
+}
+
+
+int main()
+{
+    return 0;
+}
--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -3,13 +3,19 @@
 #include <iomanip>
 using namespace std;
 
-float floatTest(int x);
-{
-    
-}
-
 int main()
 {
+    int x;
+    
+    string word = "Statement";
+    
+    double num = 6 * num; 
+    
+    char choose;
+    cin >> choose;
+
+    int value = 5;
+    cout << value + 2 <<"\n";
+
     return 0;
 }
-
